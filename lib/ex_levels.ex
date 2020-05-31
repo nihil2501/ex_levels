047179ecd4e2a1ee1dea4468d92880d4c67b4526
Oren Mittman <nihil2501@gmail.com>
2020-05-31 17:21:42 -0400

    Initial commit

---
+++ lib/ex_levels.ex
@@ -0,0 +1,18 @@
+defmodule ExLevels do
+  @moduledoc """
+  Documentation for `ExLevels`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExLevels.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_levels_test.exs
@@ -0,0 +1,8 @@
+defmodule ExLevelsTest do
+  use ExUnit.Case
+  doctest ExLevels
+
+  test "greets the world" do
+    assert ExLevels.hello() == :world
+  end
+end
